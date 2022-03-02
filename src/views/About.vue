6d2441f5ce1b845ee1da97cce3cf2b1f01dc3514
Scenaristeur <scenaristeur@gmail.com>
2022-03-02 11:58:10 +0100

    about

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,6 +2,10 @@
   <div class="about">
     <h1>This is an about page</h1>
 
+    on a computer, use arrows : right/left and down to throw the ball
+    <br>
+    on a smartphine, tap on top toi throw the ball, right bottom and left bottom to use the flips
+
     <a href="https://github.com/scenaristeur/flipball" target="_blank">Source scenaristeur github</a>
   </div>
 </template>
